227827bd72fa7fdb5d3b8a2addb2712c593e0b0e
“Mohammed <4thkiller2005@gmail.com>
2025-08-25 19:16:35 +0000

    Update

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-    int sum = 0;
-    int i;
+	int sum = 0;
+	int i;
 
-    for (i = 1; i < 1024; i++)
-    {
-        if (i % 3 == 0 || i % 5 == 0)
-        {
-            sum += i;
-        }
-    }
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
 
-    printf("%d\n", sum);
-    return (0);
+	printf("%d\n", sum);
+	return (0);
 }
