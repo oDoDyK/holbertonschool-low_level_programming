1372240d4a0f53dac18ac044a04bc6dfb2577231
“Mohammed <4thkiller2005@gmail.com>
2025-08-25 18:01:02 +0000

    Update

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the lowercase alphabet followed by a new line
+ */
+void print_alphabet(void)
+{
+    char c = 'a';
+
+    while (c <= 'z')
+        _putchar(c++);
+    _putchar('\n');
+}
