a324e58127fd62c0f0159f2c8fdcedd7fc0a1abf
“Mohammed <4thkiller2005@gmail.com>
2025-08-25 16:21:09 +0000

    Update

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,30 +3,30 @@
 #include <stdio.h>
 
 /**
- * main - assigns a random number to n and prints if it is
- *        postive, zero, negative
- * Return: always 0
+ * main - assigns a random number to n and prints whether
+ *        it is positive, negative, or zero
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int n;
+	int n;
 
-/* DON'T TOUCH this seeding / random code */
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-    if (n > 0)
-    {
-    printf("%d is positive\n", n);
-    }
-    else if (n == 0)
-    {
-    printf("%d is zero\n", n);
-    }
-    else
-    {
-    printf("%d is negative\n", n);
-    }
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
 
-    return (0);
+	return (0);
 }
