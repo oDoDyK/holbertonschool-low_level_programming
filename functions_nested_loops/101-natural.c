5b9c6df43b87f0915a9b3703e0b4b6320fc012af
“Mohammed <4thkiller2005@gmail.com>
2025-08-25 19:07:24 +0000

    Update

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,16 +1,18 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * natural_sum - Prints the sum of all multiples of 3 or 5 below 1024
- */
-void natural_sum(void)
+int main(void)
 {
-    int i, sum = 0;
+    int sum = 0;
+    int i;
 
-    for (i = 0; i < 1024; i++)
+    for (i = 1; i < 1024; i++)
     {
-        if ((i % 3 == 0) || (i % 5 == 0))
+        if (i % 3 == 0 || i % 5 == 0)
+        {
             sum += i;
+        }
     }
+
     printf("%d\n", sum);
+    return (0);
 }
