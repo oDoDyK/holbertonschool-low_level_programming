1eca11312289ea0f1d6119999d2dd1b4a42556c3
“Mohammed <4thkiller2005@gmail.com>
2025-08-25 18:28:37 +0000

    Update

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,22 +1,12 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-void jack_bauer(void)
+int main(void)
 {
-    int h, m;
-
-    for (h = 0; h < 24; h++)
-    {
-        for (m = 0; m < 60; m++)
-        {
-            _putchar('0' + h / 10);
-            _putchar('0' + h % 10);
-            _putchar(':');
-            _putchar('0' + m / 10);
-            _putchar('0' + m % 10);
-            _putchar('\n');
-        }
-    }
+	jack_bauer();
+	return (0);
 }
