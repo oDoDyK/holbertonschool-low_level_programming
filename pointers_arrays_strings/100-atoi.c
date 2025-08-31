ae5941001da92b001e76c68f58e0e05510f5e2da
“Mohammed <4thkiller2005@gmail.com>
2025-08-31 14:07:20 +0000

    update

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,34 +8,34 @@
  */
 int _atoi(char *s)
 {
-    int sign = 1;
-    int result = 0;
-    int i = 0;
-    int digit;
+	int sign = 1;
+	int result = 0;
+	int i = 0;
+	int digit;
 
-    /* Skip non-digit characters and process signs */
-    while (s[i] && (s[i] < '0' || s[i] > '9'))
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        else if (s[i] == '+')
-            sign *= 1;
-        i++;
-    }
+	/* Skip non-digit characters and process signs */
+	while (s[i] && (s[i] < '0' || s[i] > '9'))
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] == '+')
+			sign *= 1;
+		i++;
+	}
 
-    /* Convert digits to integer with overflow check */
-    while (s[i] >= '0' && s[i] <= '9')
-    {
-        digit = s[i] - '0';
-        if (result > (2147483647 - digit) / 10)
-        {
-            if (sign == 1)
-                return (2147483647);
-            return (-2147483648);
-        }
-        result = result * 10 + digit;
-        i++;
-    }
+	/* Convert digits to integer with overflow check */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		digit = s[i] - '0';
+		if (result > (2147483647 - digit) / 10)
+		{
+			if (sign == 1)
+				return (2147483647);
+			return (-2147483648);
+		}
+		result = result * 10 + digit;
+		i++;
+	}
 
-    return (sign * result);
+	return (sign * result);
 }
