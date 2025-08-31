4ad6b3ff8603ea46d8ececef609d7f3c66667412
“Mohammed <4thkiller2005@gmail.com>
2025-08-31 16:08:11 +0000

    update

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Description: Sets a[2] to 98 using pointer arithmetic
- * Author: Student
- * Return: Always 0 (Success)
- */
+
 int main(void)
 {
-    int a[5];
-    int *p;
-    p = a;
-    /* Add code below this line */
-    *(p + 2) = 98;
-    printf("a[2] = %d\n", a[2]);
-    return (0);
+  int n;
+  int a[5];
+  int *p;
+
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to use ,
+   * - Do not remove anything from the initial code
+   * (not even the comments)
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints a[2] = 98 */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
