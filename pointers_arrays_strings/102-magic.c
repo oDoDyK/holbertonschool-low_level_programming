d0d68416c1e4938c41fc971a7e5b7c216687c234
“Mohammed <4thkiller2005@gmail.com>
2025-08-31 16:05:15 +0000

    update

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -11,12 +10,9 @@ int main(void)
 {
     int a[5];
     int *p;
-
     p = a;
-
     /* Add code below this line */
     *(p + 2) = 98;
-
     printf("a[2] = %d\n", a[2]);
     return (0);
 }
