1afa12c9c9bd86bf0b179ca35d0aa522e5c4851d
“Mohammed <4thkiller2005@gmail.com>
2025-08-31 16:16:18 +0000

    update

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
-
-void magic(void)
-{
-    int a[5];  /* Array to work with */
-    int *p;     /* Pointer to manipulate the array */
-
-    a[2] = 0;   /* Initialize a[2] to some value */
-    p = a;      /* p points to the start of array a */
-
-    /* Line 19: Add the required statement here */
-    *(p + 2) = 98; /* Set a[2] to 98 using pointer arithmetic */
-    ;
-
-    /* Existing print statement to output the result */
-    printf("a[2] = %d\n", a[2]);
-}
-
 int main(void)
 {
-    magic();
-    return (0);
+  int n;
+  int a[5];
+  int *p;
+  a[2] = 1024;
+  p = a; /* Adjusted to point to a, as p = &n doesn’t allow accessing a[2] */
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 2) = 98; /* Line 19: Sets a[2] to 98 */
+  ;
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
