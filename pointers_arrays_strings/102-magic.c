acd61985e76fea4996832109a129d0fcdda8ddeb
“Mohammed <4thkiller2005@gmail.com>
2025-08-31 15:53:00 +0000

    update

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -1,19 +1,24 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main - check the code
+ * main - Entry point
  *
- * Return: Always 0.
+ * Description: Program to demonstrate pointer arithmetic
+ *              Sets a[2] to 98 using a pointer
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int a[5] = {0, 1, 98, 3, 4};
+    int a[5];
     int *p;
 
     p = a;
 
-    printf("a[2] = %d\n", *(p + 2));
+    /* Set a[2] to 98 without using variable a */
+    
+    *(p + 2) = 98;
 
+    printf("a[2] = %d\n", a[2]);
+    
     return (0);
 }
