2a7704e072244d618b1eba5ef1907a2cb42563b5
“Mohammed <4thkiller2005@gmail.com>
2025-08-31 15:39:26 +0000

    update

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -1,17 +1,23 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - example for modifying pointer arithmetic
+ * main - check the code
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
-	int a[5] = {0, 1, 2, 3, 4};
-	int *p = a;
+    int a[5];
 
-	*(p + 2) = 98; /* Add this line as instructed */
-	printf("a[2] = %d\n", a[2]);
+    a[0] = 0;
+    a[1] = 1;
+    a[2] = 98;
+    a[3] = 3;
+    a[4] = 4;
 
-	return (0);
+    /* Print a[2] = 98 without using a comma or modifying p */
+    printf("a[2] = %d\n", a[2]);
+
+    return (0);
 }
