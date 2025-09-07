48bc945bf0b9faa6a9ea2ee11cf8177d25300021
“Mohammed <4thkiller2005@gmail.com>
2025-09-07 13:54:53 +0000

    update

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -2,19 +2,18 @@
 
 /**
  * _memset - fills memory with a constant byte
- * @s: pointer to the memory area
- * @b: the constant byte to fill with
+ * @s: pointer to memory area
+ * @b: constant byte to fill
  * @n: number of bytes to fill
  *
- * Return: pointer to the memory area s
+ * Return: pointer to memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
     unsigned int i;
-    
+
     for (i = 0; i < n; i++)
-    {
-       s[i] = b;
-    }
+        s[i] = b;
+
     return (s);
 }
