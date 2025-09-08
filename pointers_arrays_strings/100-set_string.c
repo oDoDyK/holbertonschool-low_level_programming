acff4ca9b754f9957d4687481483b38d7a27af97
“Mohammed <4thkiller2005@gmail.com>
2025-09-08 15:39:54 +0000

    update

--- pointers_arrays_strings/100-set_string.c
+++ pointers_arrays_strings/100-set_string.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: double pointer to a char
+ * @to: pointer to a char to assign
+ *
+ * Description: This function makes *s point to the same location as 'to'.
+ */
 void set_string(char **s, char *to)
 {
 *s = to;
