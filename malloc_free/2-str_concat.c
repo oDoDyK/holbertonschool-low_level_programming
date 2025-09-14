3fb3f472377599db92995ae58a3310ec77942a0b
“Mohammed <4thkiller2005@gmail.com>
2025-09-14 16:41:19 +0000

    update

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings.
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: pointer to the newly allocated space in memory
+ */
+char *str_concat(char *s1, char *s2)
+{
+int counter1, counter2, i, j;
+char *strTemp;
+
+counter1 = 0;
+counter2 = 0;
+
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+while (s1[counter1] != '\0')
+{
+counter1++;
+}
+while (s2[counter2] != '\0')
+{
+counter2++;
+}
+counter2++;
+strTemp = malloc(sizeof(char) * (counter1 + counter2));
+if (strTemp == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < counter1; i++)
+{
+strTemp[i] = s1[i];
+}
+for (j = 0; j < counter2; j++)
+{
+strTemp[i] = s2[j];
+}
+return (strTemp);
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -7,4 +7,7 @@ char *create_array(unsigned int size, char c);
 /* 1-strdup.c */
 char *_strdup(char *str);
 
+/* 2-str_concat.c */
+char *str_concat(char *s1, char *s2);
+
 #endif
