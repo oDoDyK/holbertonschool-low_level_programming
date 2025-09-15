038416a0734b0464270a3d31e1dff8e1826f8992
“Mohammed <4thkiller2005@gmail.com>
2025-09-15 16:32:48 +0000

    update

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -10,4 +10,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 /* 2-calloc.c */
 void *_calloc(unsigned int nmemb, unsigned int size);
 
+/* 3-array_range.c */
+int *array_range(int min, int max);
+
 #endif /* MAIN_H */
