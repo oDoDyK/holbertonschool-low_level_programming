d12225e6758532b80e08838bd193588377fd371b
“Mohammed <4thkiller2005@gmail.com>
2025-09-15 16:44:26 +0000

    update

---
+++ more_malloc_free/101-mul.c
@@ -0,0 +1,107 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _isdigit - checks if a string contains only digits
+ * @s: string to check
+ *
+ * Return: 1 if all digits, 0 otherwise
+ */
+int _isdigit(char *s)
+{
+	while (*s)
+	{
+		if (*s < '0' || *s > '9')
+			return (0);
+		s++;
+	}
+	return (1);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+/**
+ * errors - prints Error and exits with status 98
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * multiply - multiplies two positive numbers
+ * @num1: first number
+ * @num2: second number
+ */
+void multiply(char *num1, char *num2)
+{
+	int len1, len2, len, i, j, carry, n1, n2, *result;
+	int start = 0;
+
+	len1 = _strlen(num1);
+	len2 = _strlen(num2);
+	len = len1 + len2;
+
+	result = calloc(len, sizeof(int));
+	if (result == NULL)
+		exit(98);
+
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		n1 = num1[i] - '0';
+		carry = 0;
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			n2 = num2[j] - '0';
+			carry += result[i + j + 1] + (n1 * n2);
+			result[i + j + 1] = carry % 10;
+			carry /= 10;
+		}
+		result[i + j + 1] += carry;
+	}
+
+	while (start < len && result[start] == 0)
+		start++;
+
+	if (start == len)
+		_putchar('0');
+	else
+		for (; start < len; start++)
+			_putchar(result[start] + '0');
+
+	_putchar('\n');
+	free(result);
+}
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+		errors();
+	if (!_isdigit(argv[1]) || !_isdigit(argv[2]))
+		errors();
+
+	multiply(argv[1], argv[2]);
+	return (0);
+}
--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -16,4 +16,11 @@ int *array_range(int min, int max);
 /* 100-realloc.c */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
+/* 101-mul.c */
+int _putchar(char c);
+int _isdigit(char *s);
+int _strlen(char *s);
+void errors(void);
+void multiply(char *num1, char *num2);
+
 #endif /* MAIN_H */
