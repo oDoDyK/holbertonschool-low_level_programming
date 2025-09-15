5c6880345c66de17c12239afd562380733c6be81
“Mohammed <4thkiller2005@gmail.com>
2025-09-15 17:07:15 +0000

    update

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -4,9 +4,12 @@
 
 /**
  * print_error - Prints "Error" and exits with status 98
+ * @ptr: Pointer to free before exiting (can be NULL)
  */
-void print_error(void)
+void print_error(char *ptr)
 {
+	if (ptr)
+		free(ptr);
 	printf("Error\n");
 	exit(98);
 }
@@ -31,10 +34,31 @@ int is_valid_number(char *str)
 }
 
 /**
+ * trim_leading_zeros - Allocates a new string without leading zeros
+ * @str: Input string
+ * Return: Newly allocated string, or NULL if malloc fails
+ */
+char *trim_leading_zeros(char *str)
+{
+	int i = 0;
+	char *result;
+
+	while (str[i] == '0' && str[i + 1] != '\0')
+		i++;
+
+	result = malloc(strlen(str + i) + 1);
+	if (!result)
+		return (NULL);
+
+	strcpy(result, str + i);
+	return (result);
+}
+
+/**
  * multiply_numbers - Multiplies two positive numbers as strings
  * @num1: First number string
  * @num2: Second number string
- * Return: Dynamically allocated result string
+ * Return: Dynamically allocated result string, or NULL on malloc fail
  */
 char *multiply_numbers(char *num1, char *num2)
 {
@@ -42,6 +66,7 @@ char *multiply_numbers(char *num1, char *num2)
 	int result_len = len1 + len2;
 	char *result = malloc(result_len + 1);
 	int i, j, n1, n2, carry, sum;
+	char *final_result;
 
 	if (!result)
 		return (NULL);
@@ -64,11 +89,10 @@ char *multiply_numbers(char *num1, char *num2)
 		result[i + j + 1] += carry;
 	}
 
-	i = 0;
-	while (i < result_len - 1 && result[i] == '0')
-		i++;
+	final_result = trim_leading_zeros(result);
+	free(result);
 
-	return (strdup(result + i));
+	return (final_result);
 }
 
 /**
@@ -79,17 +103,17 @@ char *multiply_numbers(char *num1, char *num2)
  */
 int main(int argc, char *argv[])
 {
-	char *result;
+	char *result = NULL;
 
 	if (argc != 3)
-		print_error();
+		print_error(NULL);
 
 	if (!is_valid_number(argv[1]) || !is_valid_number(argv[2]))
-		print_error();
+		print_error(NULL);
 
 	result = multiply_numbers(argv[1], argv[2]);
 	if (!result)
-		print_error();
+		print_error(NULL);
 
 	printf("%s\n", result);
 	free(result);
