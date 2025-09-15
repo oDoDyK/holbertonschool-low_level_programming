339d4c1de5e2a828315bb7624c81dfad18d87641
“Mohammed <4thkiller2005@gmail.com>
2025-09-15 16:28:27 +0000

    update

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -7,4 +7,7 @@ void *malloc_checked(unsigned int b);
 /* 1-string_nconcat.c */
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 
+/* 2-calloc.c */
+void *_calloc(unsigned int nmemb, unsigned int size);
+
 #endif /* MAIN_H */
