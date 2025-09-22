9d690166a82b41a4c8e399a20bf184492649fcc5
“Mohammed <4thkiller2005@gmail.com>
2025-09-22 15:26:25 +0000

    Update

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -9,4 +9,7 @@ void print_name(char *name, void (*f)(char *));
 /* 1-array_iterator */
 void array_iterator(int *array, size_t size, void (*action)(int));
 
+/* 2-int_index.c */
+int int_index(int *array, int size, int (*cmp)(int));
+
 #endif
