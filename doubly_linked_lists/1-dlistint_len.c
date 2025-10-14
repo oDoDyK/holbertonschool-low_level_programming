a04bd3d2e42cdfad92ef98659c335f610975ea41
“Mohammed <4thkiller2005@gmail.com>
2025-10-14 12:31:12 +0000

    Update

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,4 @@
 	#include "lists.h"
-	
 	/**
 	* dlistint_len - Counts the number of elements in a linked dlistint_t list.
 	* @h: The head of the dlistint_t list.
@@ -9,13 +8,12 @@
 	size_t dlistint_len(const dlistint_t *h)
 	{
 		size_t nodes = 0;
-	
+
 		while (h)
 		{
 			nodes++;
 			h = h->next;
 		}
-	
 		return (nodes);
 	}
-	
+
