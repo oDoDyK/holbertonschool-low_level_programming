5f0c61d6d975c7e41f7d592c72d58f8ae3b6124e
“Mohammed <4thkiller2005@gmail.com>
2025-10-15 14:55:24 +0000

    Update

--- file_io/main.h
+++ file_io/main.h
@@ -12,4 +12,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 /* 1-create_file.c */
 int create_file(const char *filename, char *text_content);
 
+/* 2-append_text_to_file.c */
+int append_text_to_file(const char *filename, char *text_content);
+
 #endif
